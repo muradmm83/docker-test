ec6d36be0175173a2eeb2f1bd130c086aabdb69d
muradmm83 <muradmm83@gmail.com>
2024-01-11 05:48:32 +0300

    Add nodemon package

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000
 app.use(express.json())
 
 app.get('/', (_, res) => {
-    res.send('Hello World 😊!')
+    res.send('Hello, World ✌️')
 })
 
 app.use('/api/todos', todosController)
