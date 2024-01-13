7f00f418e168a39391a3cfdab548ac05e38f33da
muradmm83 <muradmm83@gmail.com>
2024-01-14 01:32:05 +0300

    Add database connection

---
+++ src/db.ts
@@ -0,0 +1,8 @@
+import { connect, connection } from 'mongoose'
+
+connect(`mongodb://${process.env.MONGO_USERNAME || 'murad'}:${process.env.MONGO_PWD || 'murad'}@localhost:27017`);
+
+connection.on('error', console.error.bind(console, 'MONGO> connection error:'));
+connection.once('open', function () {
+    console.log('MONGO> Database connected successfully');
+});
