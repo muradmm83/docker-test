fa54125afc89cfc804043ea910fb21fd465790c1
muradmm83 <muradmm83@gmail.com>
2024-01-16 02:03:09 +0300

    Use environment variable

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { connect, connection } from 'mongoose'
 
-connect(`mongodb://${process.env.MONGO_USERNAME || 'murad'}:${process.env.MONGO_PWD || 'murad'}@localhost:27017`);
+connect(`mongodb://${process.env.MONGO_USERNAME || 'murad'}:${process.env.MONGO_PWD || 'murad'}@${process.env.MONGO_HOST}:27017`);
 
 connection.on('error', console.error.bind(console, 'MONGO> connection error:'));
 connection.once('open', function () {
