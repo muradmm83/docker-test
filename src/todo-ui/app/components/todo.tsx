33304bb5033386c3d371d7c6ce7ba9486c3c5513
muradmm83 <muradmm83@gmail.com>
2024-07-25 15:46:43 +0300

    Add: change to client component

--- src/todo-ui/app/components/todo.tsx
+++ src/todo-ui/app/components/todo.tsx
@@ -1,22 +1,46 @@
+'use client';
+
 import clsx from "clsx";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { MdOutlineRadioButtonUnchecked, MdDeleteForever } from "react-icons/md";
+
 interface TodoProps {
+  _id: string;
   title: string;
   desc: string;
   completed?: boolean;
 }
 
-export default function Todo({ title, desc, completed = false }: TodoProps) {
+export default function Todo({
+  _id,
+  title,
+  desc,
+  completed = false,
+}: TodoProps) {
   return (
-    <div className={clsx("rounded p-4 bg-neutral-300", {
-      '!bg-green-300': completed
-    })}>
-      <label className="text-2xl">{title}</label>
-      <div className="flex items-stretch">
-        <p className="grow">{desc}</p>
-        <span className="self-center">
-          {completed ? "Completed" : "Pending"}
-        </span>
+    <div
+      className={clsx("flex items-center my-4", {
+        "text-gray-400": completed,
+      })}
+    >
+      <button
+        className={clsx("justify-self-start", {
+          "text-green-500": completed,
+        })}
+      >
+        {completed ? <FaRegCheckCircle /> : <MdOutlineRadioButtonUnchecked />}
+      </button>
+      <div className="flex-1 mx-4">
+        <label>{title}</label>
+        <p className="grow text-xs">{desc}</p>
       </div>
+      <button
+        className="justify-self-end text-sky-500 text-2xl"
+        title="Delete"
+        onClick={() => alert(`Removing ${_id}`)}
+      >
+        <MdDeleteForever />
+      </button>
     </div>
   );
 }
