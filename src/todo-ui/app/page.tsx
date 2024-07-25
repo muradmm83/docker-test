785350fa418268b3d6d6454d7ecab7921618b7d0
muradmm83 <muradmm83@gmail.com>
2024-07-25 15:45:01 +0300

    Refactor: use components

--- src/todo-ui/app/page.tsx
+++ src/todo-ui/app/page.tsx
@@ -1,19 +1,11 @@
 import TodoList from "@/app/components/todo-list";
+import CreateTodoForm from "@/app/components/create-todo";
 
 export default function Home() {
   return (
-    <main className="md:container md:mx-auto md:w-2/5 grid grid-cols-1 gap-2">
-      <div className="grid gap-1 grid-cols-4 p-2 rounded border-2 border-slate-300">
-        <label className="col-span-4 text-xl">New TODO</label>
-        <label>Title</label>
-        <input type="text" className="border-2 col-span-3 border-slate-300 focus:border-sky-300 default:border-sky-300 outline-none" />
-        <label>Description</label>
-        <textarea className="border-2 col-span-3 border-slate-300  focus:border-sky-300 default:border-sky-300 outline-none"></textarea>
-        <button className="col-span-4 rounded  text-sky-300 hover:bg-sky-300 hover:text-white justify-self-end">
-          Create
-        </button>
-      </div>
+    <main className="md:container md:mx-auto md:w-2/5 p-1 bg-white h-screen rounded">
       <TodoList />
+      <CreateTodoForm />
     </main>
   );
 }
